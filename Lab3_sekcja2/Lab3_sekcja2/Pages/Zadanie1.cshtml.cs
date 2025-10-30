fec31cc77c1e9719b2e82e81a0a5f49d34e1bd8f
Morswin <41349347+Morswin@users.noreply.github.com>
2025-10-30 12:14:55 +0100

    Initial project setup for Lab3_sekcja2
    Add initial ASP.NET project files, configuration, and dependencies for Lab3_sekcja2. Includes solution, project files, Razor pages, static assets, and required libraries (Bootstrap, jQuery, validation).

---
+++ Lab3_sekcja2/Lab3_sekcja2/Pages/Zadanie1.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Lab3_sekcja2.Pages
+{
+    public class Zadanie1Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
